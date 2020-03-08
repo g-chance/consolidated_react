6aafa76b0906865665e30a02b7d4f7f944f49310
Greg Chance <ge.chance@gmail.com>
2020-03-07 21:40:44 -0800

    updates

--- react_routing/src/App.js
+++ react_routing/src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 import Home from './components/Home';
 import Adv from './components/Adv'
 import Word from './components/Word'
+import Index from './components/Index'
 
 function App() {
   return (
     <div className="wrapper">
+      <Index></Index>
       <Router>
-        <Home path='/home'></Home>
+        {/* <Home path='/home'></Home>
         <Word path='/:id'></Word>
-        <Adv path='/:word/:color/:bgColor' ></Adv>
+        <Adv path='/:word/:color/:bgColor' ></Adv> */}
       </Router>
     </div>
   );
--- react_routing/src/components/Home.js
+++ react_routing/src/components/Home.js
@@ -5,6 +5,7 @@ const Home = (props) => {
     return (
         <div>
             <h1>Home</h1>
+            {props.childen}
         </div>
     )
 }
